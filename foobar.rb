0c2420a94f9de0f0dc768ec777bc976388dbba62
David Liu <daveliu@berkeley.edu>
2015-09-17 18:18:37 -0700

    Finished hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,7 @@
 class Foobar
-  # Q4 CODE HERE
+    # Q4 CODE HERE
+    # Write a class function in foobar.rb called baz that takes in an array of integers as strings, changes each string into an integer, adds two to each number, keeps the even numbers only, takes out any duplicates, rejects the resulting numbers greater than ten, and returns the sum of the resulting array.
+    def self.baz(a)
+        return a = a.map{|e| (e.tr(',','').to_i + 2)}.uniq.select{|num| num.even? && num < 10}.reduce(:+)
+    end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,18 +1,27 @@
 def squared_sum(a, b)
-  k = 0
-  while (k != 0)
+    return (a+b) ** 2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+    return a.sort.map! {|e| e+1}
+    
+    # Merge Sort for fun-zies
+    # if a.count <= 1
+    #     a[0] += 1
+    #     return a
+    # middle  = lists[0..(lists.count / 2) - 1 ]
+    # left = lists[0..middle.count - 1]
+    # right = lists[middle.count..lists.count]
+    # x = merge_sort(left)
+    # y = merge_sort(right)
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+    return first_name + " " + last_name
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+    # DO NOT EDIT THIS CODE BELOW
+    require './foobar'
+    Foobar.baz a
 end
